49a183a54a133da053285eae45330d1106d742cf
vaibdix <dixitvaib@gmail.com>
2024-12-02 00:47:57 +0530

    day 1 part 2

--- day1/solve.js
+++ day1/solve.js
@@ -15,3 +15,12 @@ const a = input.map(pair => pair[0]).sort((a, b) => a - b);
 const b = input.map(pair => pair[1]).sort((a, b) => a - b);
 const total = a.reduce((sum, val, i) => sum + Math.abs(val - b[i]), 0);
 console.log('Part 1:', total);
+
+// Part 2
+const bFreq = b.reduce((counter, num) => {
+  counter[num] = (counter[num] || 0) + 1;
+  return counter;
+}, {});
+
+const similarity = a.reduce((sum, num) => sum + (bFreq[num] || 0) * num, 0);
+console.log('Part 2:', similarity);
