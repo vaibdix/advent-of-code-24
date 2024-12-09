4b16f394c2e3e1cde61632ea9434e7cec9a0a21e
vaibdix <dixitvaib@gmail.com>
2024-12-09 15:25:59 +0530

    day 8 part 2

--- day8/solve.js
+++ day8/solve.js
@@ -46,3 +46,40 @@ for (const [_, locations] of antennas1) {
 }
 console.log('Part 1:', antinodes1.size);
 
+// Part 2
+const antennas2 = new Map();
+for (let y = 0; y < input.length; y++) {
+    for (let x = 0; x < input[0].length; x++) {
+        if (input[y][x] !== '.') {
+            if (!antennas2.has(input[y][x])) {
+                antennas2.set(input[y][x], []);
+            }
+            antennas2.get(input[y][x]).push([x, y]);
+        }
+    }
+}
+
+const antinodes2 = new Set();
+for (const [_, locations] of antennas2) {
+    for (let i = 0; i < locations.length; i++) {
+        for (let j = i + 1; j < locations.length; j++) {
+            const [x1, y1] = locations[i];
+            const [x2, y2] = locations[j];
+
+            for (let y = 0; y < input.length; y++) {
+                for (let x = 0; x < input[0].length; x++) {
+                    if ((x === x1 && y === y1) || (x === x2 && y === y2)) continue;
+                    if (isCollinear(x1, y1, x2, y2, x, y)) {
+                        antinodes2.add(`${x},${y}`);
+                    }
+                }
+            }
+
+            if (locations.length > 1) {
+                antinodes2.add(`${x1},${y1}`);
+                antinodes2.add(`${x2},${y2}`);
+            }
+        }
+    }
+}
+console.log('Part 2:', antinodes2.size);
